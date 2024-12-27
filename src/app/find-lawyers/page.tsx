c5e1773f29c2e3b4f8bc830362631a0f6e02b70c
lonnewolf120 <iftekharulislam360@gmail.com>
2024-12-28 02:28:31 +0600

    .

--- src/app/find-lawyers/page.tsx
+++ src/app/find-lawyers/page.tsx
@@ -1,17 +1,16 @@
+'use client'
 import { FindLawyersMap } from '@/components/find-lawyers-map'
 import { LawyerSearch } from '@/components/lawyer-search'
 import dynamic from 'next/dynamic'
 
-const DynamicFindLawyersMap = dynamic(() => import('@/components/find-lawyers-map').then(mod => mod.FindLawyersMap), {
-  ssr: false
-})
+// import {FindLawyersMap} from '@/components/find-lawyers-map'
 
 export default function FindLawyersPage() {
   return (
     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
       <h1 className="text-3xl font-serif mb-8">Find Lawyers</h1>
       <div className="grid lg:grid-cols-[2fr,1fr] gap-8">
-        <DynamicFindLawyersMap />
+        <FindLawyersMap />
         <LawyerSearch />
       </div>
     </div>
