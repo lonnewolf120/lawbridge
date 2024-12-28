8389a3f0fe18fe1d6ca74cb1fc4a86292a29317a
lonnewolf120 <iftekharulislam360@gmail.com>
2024-12-28 18:33:18 +0600

    update and add blogs

--- src/app/find-lawyers/page.tsx
+++ src/app/find-lawyers/page.tsx
@@ -3,6 +3,7 @@ import { LawyerSearch } from '@/components/lawyer-search'
 import { FindLawyersMap } from '@/components/find-lawyers-map'
 export default function FindLawyersPage() {
   return (
+    <main className='min-h-screen'>
     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
       <h1 className="text-3xl font-serif mb-8">Find Lawyers</h1>
       <div className="grid lg:grid-cols-[2fr,1fr] gap-8">
@@ -10,5 +11,6 @@ export default function FindLawyersPage() {
         <LawyerSearch />
       </div>
     </div>
+    </main>
   )
 }
--- src/components/legal-category-form.tsx
+++ src/components/legal-category-form.tsx
@@ -53,13 +53,13 @@ export function LegalCategoryForm({
           </div>
           <div className="space-y-2">
             <Label htmlFor="issue-description">Briefly describe your legal issue </Label>
-            <button
+            {/* <button
               type="button"
               className="ml-2 rounded-full bg-gray-200 p-1 text-gray-500 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
               aria-label="Help with description"
               onClick={() => {
                 // Logic to show the popup
-                
+                showPopup = !showPopup;
 
               }}
             >
@@ -77,13 +77,13 @@ export function LegalCategoryForm({
                   d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
                 />
               </svg>
-            </button>
-            <div className={`bg-blue-100 text-amber-800 rounded-md text-sm ${showPopup ? 'text-opacity-80' : 'text-opacity-0'} p-2`}>
+            </button> */}
+             {/* <div className={`text-amber-800 rounded-md text-sm ${showPopup ? 'text-opacity-80' : 'text-opacity-0'} p-2`}>
                   <p>this gives the AI assistant more insight about your matter and so his discussions will remain within context</p>
-                </div>
+                </div> */}
             <Textarea
               id="issue-description"
-              placeholder="Enter your legal issue here..."
+              placeholder="this gives the AI assistant more insight about your matter and so his discussions will remain within context"
               value={description || ''}
               onChange={(e) => setDescription(e.target.value)}
             />
